bb00ea11bb762a0eef3b00e67de90012bba76c44
Zordoron Rez <Zordoron1506hmu@gmail.com>
2023-10-11 21:24:46 +0900

    hi

--- BatShortcuts/commit_shortcut.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set /p commit_message=<commit_msg.txt
-git add .
-git commit -m "%commit_message%"
-git push
--- commit_shortcut.bat
+++ commit_shortcut.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set /p commit_message=<commitmsg.txt
+set /p commit_message=<commit_msg.txt
 git add .
 git commit -m "%commit_message%"
 git push
